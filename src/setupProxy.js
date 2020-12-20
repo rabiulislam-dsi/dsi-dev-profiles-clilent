9b0e715874a2b298e72c583342dba1128f14f5a7
Death-Gear <backsideburner@gmail.com>
2020-12-20 15:20:36 +0600

    Sets up environment variable and GetToken method for backend api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8080/api',
+            target: process.env.REACT_APP_BACKEND_API,
             changeOrigin: true,
             pathRewrite: {
                 '^/api': ''
