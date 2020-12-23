07bc7ed393e86d106155415218e9c6817a434428
Rabiul Islam Anik <rabiulislam.dsi@gmail.com>
2020-12-23 18:47:52 +0600

    Fix redundant routing issue

--- src/layout/ContainerWithNav.js
+++ src/layout/ContainerWithNav.js
@@ -1,15 +1,18 @@
 import React from "react";
 import NavWrapper from "./NavWrapper";
 import Container from "react-bootstrap/cjs/Container";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function ContainerWithNav(props){
-    console.log(props.location);
+    console.log(props);
     const renderNav=!props.location.pathname.split('/').includes('auth');
     return (
         <React.Fragment>
-            <NavWrapper renderNav={renderNav}/>
+            <NavWrapper renderNav={true}/>
             <Container fluid id="page-content-wrapper" className={renderNav?"page-content-wrapper":""}>
-                {props.children}
+                <Switch>
+                    {props.children}
+                </Switch>
             </Container>
         </React.Fragment>
     )
